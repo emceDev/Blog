f716a701f47d38f581a095d1d4ed864d798ac398
m4t1ce <48633252+m4t1ce@users.noreply.github.com>
2021-10-17 20:16:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 const dbURI = "mongodb+srv://Mateusz:Mongo1234@blogtutorial.e45gp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
+  .then(result => app.listen(443))
   .catch(err => console.log(err));
 
 // register view engine
